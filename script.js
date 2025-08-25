2a5b65f1eff805dc03e9b4eff152434ce68f04a7
VigneshJayasekaran <vkvigneshjjj0@gmail.com>
2025-08-25 12:48:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Replace with your channel ID and API Key
 const API_KEY = "AIzaSyBVS7La2SW9FILXt_CmUTowfDfIg9B881E";
-const CHANNEL_ID = "v.k.screations9706"; // Replace with your channel ID
+const CHANNEL_ID = "UCdidnvDIruEg_-MRJ6TsLsw"; // Replace with your channel ID
 const MAX_RESULTS = 6;
 
 async function loadVideos() {
@@ -42,4 +42,5 @@ async function loadVideos() {
 
 // Load on page start
 window.onload = loadVideos;
+
 
